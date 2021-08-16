1be88cf3a20acb204b69d4af91dc3ba27cbcd3b1
marty808 <helge@brust-mail.de>
2021-08-16 12:10:15 +0000

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM: caddy:2-alpine
+
+ENV PROXY_FROM
+ENV PROXY_TO
+ENV PROXY_CERT=INTERNAL
+
+COPY reverse-proxy.sh /
+CMD ./reverse-proxy.sh
---
+++ reverse-proxy.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# check for environment variables
+if [ -z $PROXY_FROM ]; then
+   echo "PROXY_FROM is empty"
+   exit 2
+fi
+
+if [ -z $PROXY_TO ]; then
+   echo "PROXY_TO is empty"
+   exit 2
+fi
+
+if [ $PROXY_CERT == "INTERNAL"]
+   tls_internal="tls internal"
+fi
+
+# write caddyfile
+cat > /etc/caddy/Caddyfile << EOF1
+$PROXY_FROM {
+  tls_internal
+  reverse_proxy $PROXY_TO  
+} 
+EOF1
+
+# start caddy
+caddy run --config /etc/caddy/Caddyfile --adapter caddyfile 
