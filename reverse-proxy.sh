4df4e245fa4bfe0d8f070d2a16bd570ae1c5818e
marty808 <helge@brust-mail.de>
2021-08-18 07:34:38 +0000

    added the capability to config by ENV

--- reverse-proxy.sh
+++ reverse-proxy.sh
@@ -1,41 +1,47 @@
 #!/bin/sh
 
-# check for environment variables
-if [ -z $PROXY_FROM ]; then
-   echo "PROXY_FROM is empty"
-   exit 2
-fi
-
-if [ -z $PROXY_TO ]; then
-   echo "PROXY_TO is empty"
-   exit 2
-fi
-
-if [ $PROXY_CERT == "INTERNAL" ]; then
-   tls_directive="tls internal"
-elif [ $PROXY_CERT == "" ]; then
-   tls_directive=""
+if [ ! -z $PROXY_CFG ]; then
+   echo "PROXY_CFG is given -> write into caddyfile"
+   echo "$PROXY_CFG" > /etc/caddy/Caddyfile
 else
-   if [ -z $PROXY_CA_CRT ]; then
-      echo "PROXY_CA_CRT is empty"
+   # check for environment variables
+   if [ -z $PROXY_FROM ]; then
+      echo "PROXY_FROM is empty"
       exit 2
    fi
-   # write cert into file
-   echo "$PROXY_CA_CRT" > /etc/caddy/ca_root.pem
 
-   tls_directive="tls {
-      ca $PROXY_CERT
-      ca_root /etc/caddy/ca_root.pem
-      }"
+   if [ -z $PROXY_TO ]; then
+      echo "PROXY_TO is empty"
+      exit 2
+   fi
+
+   if [ $PROXY_CERT == "INTERNAL" ]; then
+      tls_directive="tls internal"
+   elif [ $PROXY_CERT == "" ]; then
+      tls_directive=""
+   else
+      if [ -z $PROXY_CA_CRT ]; then
+         echo "PROXY_CA_CRT is empty"
+         exit 2
+      fi
+      # write cert into file
+      echo "$PROXY_CA_CRT" > /etc/caddy/ca_root.pem
+
+      tls_directive="tls {
+         ca $PROXY_CERT
+         ca_root /etc/caddy/ca_root.pem
+         }"
+   fi
+
+   # write caddyfile
+   cat > /etc/caddy/Caddyfile << EOF
+   $PROXY_FROM {
+   $tls_directive
+   reverse_proxy $PROXY_TO  
+   } 
+   EOF
+
 fi
-
-# write caddyfile
-cat > /etc/caddy/Caddyfile << EOF
-$PROXY_FROM {
-  $tls_directive
-  reverse_proxy $PROXY_TO  
-} 
-EOF
 
 # start caddy
 echo "starting caddy..."
